fd8a7176cc8717d485737bd98180853e0bf575c5
Daniel Sepulveda Chiguils <daniel.sepulveda@iknesoft.com>
2026-01-04 00:04:17 -0600

    Modificar tabla y modelo de usuarios

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\GendersEnum;
+use App\RoleEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,8 +16,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surnames');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string("phone_number", 12)->nullable();
+            $table->integer("age");
+            $table->enum("gender", GendersEnum::cases());
+            $table->enum("role", RoleEnum::cases())->default(RoleEnum::Client->value);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
